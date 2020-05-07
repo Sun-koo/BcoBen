abc3d11d677af32632941fa44cbf94735c5458be
유영문 <yoo4497@summitcorp.co.kr>
2020-05-07 17:51:59 +0900

    로그인 / 인증번호 입력 화면 레이아웃 작업

---
+++ app/src/main/java/kr/co/bcoben/activity/CertificateActivity.java
@@ -0,0 +1,115 @@
+package kr.co.bcoben.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import kr.co.bcoben.R;
+
+public class CertificateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static int AUTH_RESTRICT_TIME = 300;
+    private int mIntAuthTimerCount = AUTH_RESTRICT_TIME;
+
+    EditText edtNumber;
+    TextView txtTime;
+
+    Timer mTimerAuth = null;
+    Boolean mIsAuth = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certificate);
+
+        edtNumber = findViewById(R.id.edt_certificate_number);
+        txtTime = findViewById(R.id.txt_remain_time);
+        Button btnSend = findViewById(R.id.btn_send);
+        Button btnBack = findViewById(R.id.btn_back);
+        Button btnComplete = findViewById(R.id.btn_complete);
+
+        btnSend.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        btnComplete.setOnClickListener(this);
+
+        setAuthTime();
+
+        // 인증 시간 타이머
+        mTimerAuth = new Timer();
+        mTimerAuth.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.sendEmptyMessage(1);
+            }
+        }, 1000, 1000);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_send:
+                break;
+
+            case R.id.btn_back:
+                finish();
+                break;
+
+            case R.id.btn_complete:
+                break;
+        }
+    }
+
+    /**
+     * 인증 시간 타이머의 핸들러이다.
+     * 타이머에서 1초에 한번씩 핸들러에 메세지를 보내면,
+     * mIntAuthTimerCount를 하나씩 감소시킨다(이 변수의 값이 초 역활을 한다)
+     * 위의 변수가 0보다 클 때는 시간을 감소 시키며 보여주고
+     * 인증을 막기 위한 플래그를 설정한 후, Timer를 종료 시킨다.
+     */
+    Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mIntAuthTimerCount--;
+
+            if (mIntAuthTimerCount >= 0) {
+                setAuthTime();
+            } else {
+                mIsAuth = false;
+//                resetAuth();
+            }
+        }
+    };
+
+    private void setAuthTime() {
+        int min = mIntAuthTimerCount / 60;
+        int sec = mIntAuthTimerCount % 60;
+
+        String time;
+        if (sec < 10) {
+            time = String.format("0%s:0%s", min, sec);
+        } else {
+            time = String.format("0%s:%s", min, sec);
+        }
+
+        txtTime.setText(time);
+    }
+
+    private void resetAuth() {
+        if (mTimerAuth != null) {
+            mTimerAuth.cancel();
+            mTimerAuth = null;
+            mIntAuthTimerCount = AUTH_RESTRICT_TIME;
+
+            setAuthTime();
+        }
+    }
+}
--- app/src/main/java/kr/co/bcoben/activity/LoginActivity.java
+++ app/src/main/java/kr/co/bcoben/activity/LoginActivity.java
@@ -2,22 +2,82 @@ package kr.co.bcoben.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import kr.co.bcoben.R;
 
 import static kr.co.bcoben.util.CommonUtil.finishApp;
+import static kr.co.bcoben.util.CommonUtil.isIdValid;
+import static kr.co.bcoben.util.CommonUtil.isPwValid;
+import static kr.co.bcoben.util.CommonUtil.showToast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText edtId, edtPw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtId = findViewById(R.id.edt_id);
+        edtPw = findViewById(R.id.edt_pw);
+        Button btnExit = findViewById(R.id.btn_exit);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnExit.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
     }
 
     @Override
     public void onBackPressed() {
         finishApp(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_exit:
+                finishApp(this);
+                break;
+
+            case R.id.btn_login:
+                String id = edtId.getText().toString();
+                String pw = edtPw.getText().toString();
+
+                if (checkValidInput(id, pw)) {
+                    //TODO request login api
+                    Intent intent = new Intent(LoginActivity.this, CertificateActivity.class);
+                    startActivity(intent);
+                }
+                break;
+        }
+    }
+    // 아이디, 비밀번호 유효성 검사
+    private boolean checkValidInput(String id, String pw) {
+        if (id.isEmpty()) {
+            showToast(R.string.toast_input_id);
+            return false;
+        }
+
+        if (pw.isEmpty()) {
+            showToast(R.string.toast_input_pw);
+            return false;
+        }
+
+        if (!isIdValid(id)) {
+            showToast(R.string.toast_invalid_id);
+            return false;
+        }
+
+        if (!isPwValid(pw)) {
+            showToast(R.string.toast_invalid_pw);
+            return false;
+        }
+
+        return true;
     }
 }
