5893fa0ca8c04ac2ef8a9369f11b8feaccce0599
유영문 <yoo4497@summitcorp.co.kr>
2020-05-20 17:21:03 +0900

    비밀번호 재설정 화면 레이아웃 작업

--- app/src/main/java/kr/co/bcoben/activity/ResetCertificateActivity.java
+++ app/src/main/java/kr/co/bcoben/activity/ResetCertificateActivity.java
@@ -2,15 +2,133 @@ package kr.co.bcoben.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.Locale;
 
 import kr.co.bcoben.R;
+import kr.co.bcoben.component.BaseActivity;
+import kr.co.bcoben.databinding.ActivityResetCertificateBinding;
 
-public class ResetCertificateActivity extends AppCompatActivity {
+import static kr.co.bcoben.util.CommonUtil.showToast;
+import static kr.co.bcoben.util.ValidateUtil.StringPattern.ALPHA_NUM;
+import static kr.co.bcoben.util.ValidateUtil.StringPattern.CASE_ALPHA_NUM;
+import static kr.co.bcoben.util.ValidateUtil.phoneCheck;
+import static kr.co.bcoben.util.ValidateUtil.stringPatternCheck;
+
+public class ResetCertificateActivity extends BaseActivity<ActivityResetCertificateBinding> implements View.OnClickListener {
+
+    private static int AUTH_RESTRICT_TIME = 180;
+    private int authTimerCount = AUTH_RESTRICT_TIME;
+
+    private Handler authHandler = new Handler();
+    private Runnable runnable;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_certificate);
+    protected int getLayoutResource() {
+        return R.layout.activity_reset_certificate;
+    }
+
+    @Override
+    protected void initView() {
+        // 인증 시간 타이머
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                authTimerCount--;
+                if (authTimerCount > 0) {
+                    int min = authTimerCount / 60;
+                    int sec = authTimerCount % 60;
+                    String time = getString(R.string.certificate_remain_time, String.format(Locale.getDefault(), "%02d:%02d", min, sec));
+
+                    dataBinding.txtRemainTime.setText(time);
+
+                    authHandler.postDelayed(runnable, 1000);
+                } else {
+                    dataBinding.txtRemainTime.setText(R.string.certificate_remain_time_over);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_send:
+                String id = dataBinding.editId.getText().toString();
+                String phone = dataBinding.editPhone.getText().toString();
+
+                if (checkValidInputInfo(id, phone)) {
+                    showToast(R.string.toast_send_number);
+                    dataBinding.txtRemainTime.setVisibility(View.VISIBLE);
+                    startAuth();
+                }
+                break;
+
+            case R.id.btn_back:
+                finish();
+                break;
+
+            case R.id.btn_complete:
+                String number = dataBinding.editCertificateNumber.getText().toString();
+
+                if (checkValidInputCertificate(number)) {
+                    Intent intent = new Intent(ResetCertificateActivity.this, ResetPwActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.activity_finish_in, R.anim.activity_finish_out);
+    }
+
+    private void startAuth() {
+        authTimerCount = AUTH_RESTRICT_TIME;
+        authHandler.post(runnable);
+    }
+
+    private boolean checkValidInputCertificate(String number) {
+        if (authTimerCount == 0) {
+            showToast(R.string.toast_time_over_certificate_number);
+            return false;
+        }
+        if (number.isEmpty()) {
+            showToast(R.string.toast_input_certificate_number);
+            return false;
+        }
+        if (number.length() < 6) {
+            showToast(R.string.toast_input_certificate_number_length);
+            return false;
+        }
+        return true;
+    }
+
+    // 아이디, 휴대전화번호 유효성 검사
+    private boolean checkValidInputInfo(String id, String phone) {
+        if (id.isEmpty()) {
+            showToast(R.string.toast_input_id);
+            return false;
+        }
+        if (phone.isEmpty()) {
+            showToast(R.string.toast_input_phone);
+            return false;
+        }
+        if (!stringPatternCheck(id, ALPHA_NUM, 6, -1)) {
+            showToast(R.string.toast_invalid_id);
+            return false;
+        }
+        if (!phoneCheck(phone)) {
+            showToast(R.string.toast_invalid_phone);
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/kr/co/bcoben/activity/ResetPwActivity.java
@@ -0,0 +1,72 @@
+package kr.co.bcoben.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import kr.co.bcoben.R;
+import kr.co.bcoben.component.BaseActivity;
+import kr.co.bcoben.databinding.ActivityResetPwBinding;
+
+import static kr.co.bcoben.util.CommonUtil.finishApp;
+import static kr.co.bcoben.util.CommonUtil.showToast;
+import static kr.co.bcoben.util.ValidateUtil.StringPattern.CASE_ALPHA_NUM;
+import static kr.co.bcoben.util.ValidateUtil.stringPatternCheck;
+
+public class ResetPwActivity extends BaseActivity<ActivityResetPwBinding> implements View.OnClickListener {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_reset_pw;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_exit:
+                finishApp(this);
+                break;
+
+            case R.id.btn_login:
+                String pw = dataBinding.editPw.getText().toString();
+                String pwConfirm = dataBinding.editPwConfirm.getText().toString();
+
+                if (checkValidInput(pw, pwConfirm)) {
+                    Intent intent = new Intent(ResetPwActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                }
+                break;
+        }
+    }
+
+    // 아이디, 비밀번호 유효성 검사
+    private boolean checkValidInput(String pw, String pw_confirm) {
+        if (pw.isEmpty()) {
+            showToast(R.string.toast_input_pw);
+            return false;
+        }
+        if (pw_confirm.isEmpty()) {
+            showToast(R.string.toast_input_pw);
+            return false;
+        }
+
+        if (!stringPatternCheck(pw, CASE_ALPHA_NUM, 10, -1)) {
+            showToast(R.string.toast_invalid_pw);
+            return false;
+        }
+
+        if (!pw.equals(pw_confirm)) {
+            showToast(R.string.toast_not_equal_pw);
+            return false;
+        }
+        return true;
+    }
+}
