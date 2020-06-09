bcdc2f7594ae8d6f1c4862ca54e14afdad0b1e9c
유영문 <yoo4497@summitcorp.co.kr>
2020-06-09 18:42:02 +0900

    조사등록 직접입력 레이아웃 수정

--- app/src/main/java/kr/co/bcoben/adapter/MenuSelectListAdapter.java
+++ app/src/main/java/kr/co/bcoben/adapter/MenuSelectListAdapter.java
@@ -2,10 +2,14 @@ package kr.co.bcoben.adapter;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,7 +25,8 @@ public class MenuSelectListAdapter extends RecyclerView.Adapter<MenuSelectListAd
 
     private MainActivity activity;
     private List<String> list;
-    private EditText editName;
+    private EditText editName, editCount;
+    private boolean isCount;
 
     public MenuSelectListAdapter(MainActivity activity, List<String> list) {
         this.activity = activity;
@@ -42,6 +47,7 @@ public class MenuSelectListAdapter extends RecyclerView.Adapter<MenuSelectListAd
         } else {
             holder.onBind(null);
             this.editName = holder.editName;
+            this.editCount = holder.editCount;
         }
     }
 
@@ -49,9 +55,11 @@ public class MenuSelectListAdapter extends RecyclerView.Adapter<MenuSelectListAd
     public int getItemCount() {
         return list.size() + 1;
     }
-    public void setList(List<String> list) {
+    public void setList(List<String> list, boolean isCount) {
         this.list = list;
+        this.isCount = isCount;
         editName = null;
+        editCount = null;
         notifyDataSetChanged();
     }
     public String getEditName() {
@@ -61,25 +69,38 @@ public class MenuSelectListAdapter extends RecyclerView.Adapter<MenuSelectListAd
             return "";
         }
     }
+    public String getEditCount() {
+        if (editCount != null) {
+            return editCount.getText().toString();
+        } else {
+            return "";
+        }
+    }
 
     class MenuSelectHolder extends RecyclerView.ViewHolder {
 
         private View view;
         private TextView txtName;
-        private EditText editName;
+        private EditText editName, editCount;
+        private LinearLayout layoutSelf;
+        private Button btnReg;
 
         MenuSelectHolder(View view) {
             super(view);
             this.view = view;
             txtName = view.findViewById(R.id.txt_name);
             editName = view.findViewById(R.id.edit_name);
+            editCount = view.findViewById(R.id.edit_count);
+            layoutSelf = view.findViewById(R.id.layout_self);
+            btnReg = view.findViewById(R.id.btn_reg);
         }
 
         void onBind(@Nullable final String name) {
             if (name != null) {
                 txtName.setText(name);
                 txtName.setVisibility(View.VISIBLE);
-                editName.setVisibility(View.GONE);
+                layoutSelf.setVisibility(View.GONE);
+                editCount.setVisibility(View.GONE);
 
                 view.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -89,7 +110,14 @@ public class MenuSelectListAdapter extends RecyclerView.Adapter<MenuSelectListAd
                 });
             } else {
                 txtName.setVisibility(View.GONE);
-                editName.setVisibility(View.VISIBLE);
+                layoutSelf.setVisibility(View.VISIBLE);
+                editCount.setVisibility(isCount ? View.VISIBLE : View.GONE);
+                btnReg.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        activity.researchNextStep();
+                    }
+                });
             }
         }
     }
