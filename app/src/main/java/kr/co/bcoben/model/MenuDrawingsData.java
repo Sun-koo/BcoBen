abc96b591465890671aaec6f5476b28d945681c7
유영문 <yoo4497@summitcorp.co.kr>
2020-06-09 12:42:09 +0900

    프로젝트 생성 도면 리스트 데이터 모델 추가

---
+++ app/src/main/java/kr/co/bcoben/model/MenuDrawingsData.java
@@ -0,0 +1,35 @@
+package kr.co.bcoben.model;
+
+import android.net.Uri;
+
+
+public class MenuDrawingsData {
+    private String name;
+    private String facility;
+    private Uri uri;
+
+    public MenuDrawingsData(String name, String facility, Uri uri) {
+        this.name = name;
+        this.facility = facility;
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getFacility() {
+        return facility;
+    }
+    public void setFacility(String facility) {
+        this.facility = facility;
+    }
+    public Uri getUri() {
+        return uri;
+    }
+    public void setUri(Uri uri) {
+        this.uri = uri;
+    }
+}
