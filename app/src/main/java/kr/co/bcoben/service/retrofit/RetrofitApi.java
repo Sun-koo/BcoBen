24957d4ae36db4ec68807ea00735c446536167a5
유영문 <yoo4497@summitcorp.co.kr>
2020-06-10 12:51:37 +0900

    api 연동 작업 시작

--- app/src/main/java/kr/co/bcoben/service/retrofit/RetrofitApi.java
+++ app/src/main/java/kr/co/bcoben/service/retrofit/RetrofitApi.java
@@ -2,9 +2,12 @@ package kr.co.bcoben.service.retrofit;
 
 import kr.co.bcoben.model.LoginResponseData;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface RetrofitApi {
+    @FormUrlEncoded
     @POST("/app/user/login")
-    Call<LoginResponseData> userLogin();
+    Call<LoginResponseData> userLogin(@Field("user_id") String user_id, @Field("password") String password, @Field("device_id") String device_id);
 }
