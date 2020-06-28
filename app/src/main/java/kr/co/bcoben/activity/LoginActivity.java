9396f09ac7f815bd91aca05cbb04d44aa6018fe2
suku0307 <suku0307@gmail.com>
2020-06-28 19:19:31 +0900

    도면 상세 상단 스피너 처리, 입력내용 등록 처리

--- app/src/main/java/kr/co/bcoben/activity/LoginActivity.java
+++ app/src/main/java/kr/co/bcoben/activity/LoginActivity.java
@@ -45,9 +45,6 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements
     @Override
     protected void initView() {
         getDeviceId();
-        // Debug
-        dataBinding.editId.setText("bcoben2");
-        dataBinding.editPw.setText("Bcoben01!@#");
     }
 
     @Override
